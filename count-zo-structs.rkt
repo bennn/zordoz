3bcfac8a22de3f665172e543d8a3562e6bed19e3
Ben Greenman <types@ccs.neu.edu>
2014-12-08 01:09:16 -0500

    [count] data definition and provide

---
+++ count-zo-structs.rkt
@@ -0,0 +1,93 @@
+#lang racket/base
+
+;; Count the AST nodes that appear in a bytecode file
+;;
+(provide count-structs
+         zsc->string)
+
+(require compiler/zo-structs)
+
+;; [zsc] (Short for "zo-struct-counter")
+;; Each field names a zo-struct.
+;; Field names look like "num-X" where "X" is the name of the zo-struct.
+;; Each value is a natural number representing the number of times this struct appeared.
+(struct zsc
+  (num-zo
+   num-compilation-top
+   num-form
+   ;; Shapes
+   num-function-shape
+   num-struct-shape
+   num-constructor-shape
+   num-predicate-shape
+   num-accessor-shape
+   num-mutator-shape
+   num-type-shape
+   num-other-shape
+   ;;
+   num-global-bucket
+   num-module-variable
+   ;;
+   num-wrap
+   num-wrapped
+   ;;
+   num-stx
+   num-prefix
+   ;;
+   num-expr
+   ;;
+   num-provided
+   num-toplevel
+   num-seq
+   num-seq-for-syntax
+   num-inline-variant
+   num-def-values
+   num-def-syntaxes
+   num-mod
+   num-lam
+   num-closure
+   num-case-lam
+   num-let-one
+   num-let-void
+   num-install-value
+   num-let-rec
+   num-boxenv
+   num-localref
+   num-topsyntax
+   num-application
+   num-branch
+   num-with-cont-mark
+   num-beg0
+   num-splice
+   num-req
+   num-free-id-info
+   num-lexical-rename
+   num-phase-shift
+   num-wrap-mark
+   num-prune
+   num-all-from-module
+   num-nominal-path
+   num-simple-nominal-path
+   num-imported-nominal-path
+   num-phased-nominal-path
+   num-module-binding
+   num-phased-module-binding
+   num-exported-nominal-module-binding
+   num-nominal-module-binding
+   num-exported-module-binding
+   num-simple-module-binding
+   num-module-rename
+   num-top-level-rename
+   num-mark-barrier
+  )
+  #:mutable)
+
+;; Analyze input from zo-parse. Count the number of structs appearing in the bytecode.
+(define (count-structs ct)
+  ;; (-> compilation-top? zsc?)
+  (error "Not implemented"))
+
+;; Pretty-print the results of [count-structs]. 
+(define (zsc->string z)
+  ;; (-> zsc? string?)
+  (error "Not implemented"))
