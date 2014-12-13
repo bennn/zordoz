8dd3a0915b678c48b4f05c95116b3a0e9551c6af
Ben Greenman <types@ccs.neu.edu>
2014-12-12 22:45:56 -0500

    [find] idea and stub for a hack solution

--- zo-find.rkt
+++ zo-find.rkt
@@ -2,6 +2,32 @@
 
 (provide zo-find)
 
-(define (zo-find z str)
+;; -- API functions
+
+(define (zo-find z str #:limit [lim 10000])
   ;; (-> zo? string? (listof zo?))
+  ; get children, start a search at each child
+  (define results
+    (for/list ([z* (get-children z)])
+      (zo-find-aux z str 0 lim)))
+  (apply append results))
+
+;; -- private functions
+
+(define (zo-find z str i lim)
+  ;; dammit, how to do this. Need to transition and search
+  ;; OOOH for each field in zo->string output (excluding dummies)
+  ;;  add if the field's type matches [str]. Also, zo-transition into that field and
+  ;;  keep searching.
+  (cond [(< lim i) acc] ;; Search has gone too far
+        [else '()]
+  ))
+
+;; Deconstruct a printed zo to a string. TODO make this less hack.
+(define (parse-string str)
+  ;; (-> string? (cons/c string? (listof string?)))
+  (cons "" '()))
+
+(define (get-children z)
+  ;; (-> zo? (listof zo?))
   '())
