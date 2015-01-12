6bf146521310e6c1aedac760c9455eba045cf1e4
Ben Greenman <types@ccs.neu.edu>
2015-01-12 15:14:10 -0500

    [zo-find] purpose and docs

--- src/zo-find.rkt
+++ src/zo-find.rkt
@@ -1,15 +1,23 @@
 #lang racket/base
 
-(provide zo-find)
+;; Simple utility for searching zo structs.
+;; Explores the current struct's fields recursively for a exact string match.
+
+(provide
+ ;; Search a struct recursively for member zo-structs matching a string.
+ zo-find)
 
 (require (only-in racket/list empty?)
          (only-in racket/string string-split string-trim)
          (only-in compiler/zo-structs zo?)
-         "zo-transition.rkt"
+         (only-in "zo-transition.rkt" transition)
          (only-in "zo-string.rkt" zo->spec spec/c))
 
 ;; -- API functions
 
+;; Searches a zo-struct `z` recursively for member zo-structs matching the `s`.
+;; Search terminates after at most `#:limit` recursive calls.
+;; Returns a list of matching zo-structs, excluding the root struct `z`.
 (define (zo-find z str #:limit [lim 10000])
   ;; (-> zo? string? (listof zo?))
   (apply append
@@ -19,6 +27,9 @@
 
 ;; -- private functions
 
+;; Recursive helper for `zo-find`.
+;; Add the current struct to the results, if it matches.
+;; Check struct members for matches unless the search has reached its limit.
 (define (zo-find-aux z str i lim)
   (define-values (title children) (parse-zo z))
   (define results
@@ -30,7 +41,8 @@
       (cons z results)
       results))
 
-;; Return the name of the zo [z] and a list of its child zo structs.
+;; Return the name of the zo `z` and a list of its child zo structs.
+;; Uses `zo-string.rkt` to parse a raw struct.
 (define (parse-zo z)
   ;; (-> zo? (values string? (listof zo?)))
   (define z-spec     (zo->spec z))
@@ -40,6 +52,7 @@
 
 ;; Given a zo [z] and list of possible field names [strs], return the list of zo-structs
 ;; obtained by looking up each name in [strs] in the struct [z].
+;; Relies on `zo-transition.rkt` to accomplish the lookup.
 (define (get-children z strs)
   ;; (-> zo? string? (listof zo?))
   (cond [(empty? strs) '()]
