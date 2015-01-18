eb62b58bb2abcde36b8e5611209d79495b9b8c1b
Ben Greenman <types@ccs.neu.edu>
2015-01-17 20:38:24 -0500

    [pkg] added info file

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "zordoz")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define pkg-desc "REPL for exploring .zo bytecode files")
+(define version "0.6")
+(define pkg-authors '(ben))
