0f63254f214830b8ca2ebdc93cca0dde22a9862b
ben <types@ccs.neu.edu>
2015-01-23 19:25:10 -0500

    [style2] checkpoint on no-indent refactor, consider programming the tests

--- src/zo-find.rkt
+++ src/zo-find.rkt
@@ -4,6 +4,7 @@
 ;; Explores the current struct's fields recursively for a exact string match.
 
 (provide
+  ;; (-> zo? string? (listof result?))
  ;; Search a struct recursively for member zo-structs matching a string.
  zo-find
  ;; Search result: a zo-struct and the path to reach it
@@ -13,28 +14,29 @@
          (only-in racket/string string-split string-trim)
          (only-in compiler/zo-structs zo?)
          (only-in "zo-transition.rkt" transition)
-         (only-in "zo-string.rkt" zo->spec spec/c))
+         (only-in "zo-string.rkt" zo->spec spec/c)
+         racket/match)
 
 ;; -----------------------------------------------------------------------------
 
 ;; --- API functions
 
-(define-struct result (zo path))
+(struct result (zo path) #:transparent)
 
 ;; Searches a zo-struct `z` recursively for member zo-structs matching the `s`.
-;; Search terminates after at most `#:limit` recursive calls.
-;; Return a list of 'result' structs
+;; Terminates after at most `#:limit` recursive calls.
+;; Return a list of 'result' structs.
 (define (zo-find z str #:limit [lim #f])
   ;; (-> zo? string? (listof result?))
-  (apply append
-         (let-values ([(_ children) (parse-zo z)])
-           (for/list ([z* children])
-             (zo-find-aux z* '() str 1 lim '())))))
+  (define-values (_ children) (parse-zo z))
+  (apply append (for/list ([z* children]) (zo-find-aux z* '() str 1 lim '()))))
 
 ;; --- private functions
 
-;; Closures can loop. Remember them all and check for them.
+;; Check if `str` is one of the known looping zo-structs.
+;; 2015-01-23: So far as I know, only closures may loop.
 (define (may-loop? str)
+  ;; (-> string? boolean?)
   (member str (list "closure")))
 
 ;; Recursive helper for `zo-find`.
@@ -43,16 +45,19 @@
 (define (zo-find-aux z hist str i lim seen)
   (define-values (title children) (parse-zo z))
   (define results
-    (cond [(and lim (<= lim i))                    '()]
-          ;; Terminate search if we're seeing a node for the second time
-          [(and (may-loop? title) (member z seen)) '()]
-          ;; Search recursively, remember current node if we might see it again.
-          [else (define seen*   (if (may-loop? title) (cons z seen) seen))
-                (apply append
-                       (for/list ([z* children])
-                         (zo-find-aux z* (cons z hist) str (add1 i) lim seen*)))]))
+    (cond
+     [(and lim (<= lim i))
+      '()]
+     ;; Terminate search if we're seeing a node for the second time
+     [(and (may-loop? title) (member z seen))
+      '()]
+     [else
+      ;; Remember current node if we might see it again.
+      (define seen* (if (may-loop? title) (cons z seen) seen))
+      (define hist* (cons z hist))
+      (apply append (for/list ([z* children]) (zo-find-aux z* hist* str (add1 i) lim seen*)))]))
   (if (and (string=? str title) (not (member z seen)))
-      (cons (make-result z hist) results)
+      (cons (result z hist) results)
       results))
 
 ;; Return the name of the zo `z` and a list of its child zo structs.
@@ -69,14 +74,14 @@
 ;; Relies on `zo-transition.rkt` to do the lookup.
 (define (get-children z strs)
   ;; (-> zo? string? (listof zo?))
-  (cond [(empty? strs) '()]
-        [else (let-values ([(r success?) (transition z (car strs))])
-                (cond [(not success?) (get-children z (cdr strs))]
-                      [(list? r)      (append (filter zo? r)
-                                              (get-children z (cdr strs)))]
-                      [(zo?   r)      (cons r
-                                            (get-children z (cdr strs)))]))]))
-
+  (match strs
+    ['() '()]
+    [(cons hd tl)
+     (define-values (r success?) (transition z hd))
+     (cond [(not success?) (get-children z tl)]
+           [(list? r)      (append (filter zo? r) (get-children z tl))]
+           [(zo?   r)      (cons r (get-children z tl))])]))
+                
 ;; -----------------------------------------------------------------------------
 ;; --- testing
 
