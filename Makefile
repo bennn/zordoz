1043aeb817b22b2445ca353340ff7247a3acf88e
ben <types@ccs.neu.edu>
2015-01-29 03:14:11 -0500

    [gh-pages] a first approx of the docs

--- Makefile
+++
@@ -1,8 +0,0 @@
-all:
-	raco exe zordoz.rkt
-
-test:
-	raco test private/zo-find.rkt
-	raco test private/zo-shell.rkt
-	raco test private/zo-string.rkt
-	raco test private/zo-transition.rkt
--- zordoz.rkt
+++
@@ -1,8 +0,0 @@
-
-#lang racket/base
-
-;; Executing this file starts a new REPL session.
-
-(module+ main
-  (require (only-in "private/zo-shell.rkt" init))
-  (init (current-command-line-arguments)))
