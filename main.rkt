4f35e2ae9cc1397520bfc1a3c947bfcd80f2ad02
ben <types@ccs.neu.edu>
2015-01-29 01:30:02 -0500

    [scrbl] repurposed main as programming interface

--- main.rkt
+++ main.rkt
@@ -1,7 +1,20 @@
 #lang racket/base
 
-;; Executing this file starts a new REPL session.
+(require racket/contract)
 
-(module+ main
-  (require (only-in "src/zo-shell.rkt" init))
-  (init (current-command-line-arguments)))
+(require "private/zo-string.rkt"
+         "private/zo-transition.rkt"
+         "private/zo-find.rkt"
+         "private/zo-shell.rkt")
+
+(provide result result? result-zo result-path)
+(provide (contract-out
+          ;; zo-string
+          [zo->string (->* (zo?) (#:deep? boolean?) string?)]
+          [zo->spec (->i ([z zo?]) () [res (z) (and/c spec/c (specof z))])]
+          ;; zo-transition
+          [zo-transition (-> zo? string? (values (or/c zo? (listof zo?)) boolean?))
+          ;; zo-find
+          [zo-find (-> zo? string? (listof result?))]
+          ;; zo-shell
+          [init (-> (vectorof string?) void?)]))
