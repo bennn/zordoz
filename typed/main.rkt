e1539f14936060276a3bcdcdd815a5996903300e
ben <types@ccs.neu.edu>
2015-09-07 14:52:24 -0400

    [typed] add typed main.rkt

---
+++ typed/main.rkt
@@ -0,0 +1,28 @@
+#lang typed/racket/base
+
+(require compiler/zo-structs
+         zordoz/typed/private/zo-string
+         zordoz/typed/private/zo-transition
+         zordoz/typed/private/zo-find
+         zordoz/typed/private/zo-shell)
+
+(provide
+  result result? result-zo result-path
+  ;; TODO
+
+  zo->string
+  ;; TODO
+
+  zo->spec
+  ;; TODO
+
+  zo-transition
+  ;; TODO
+
+  zo-find
+  ;; TODO
+
+  zo->shell
+  ;; TODO
+
+)
